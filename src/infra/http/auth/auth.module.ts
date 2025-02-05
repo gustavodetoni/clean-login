371e966581ebf40f2d44965d60ed3af5d7eb56af
gustavodetoni <gustavodetoni2407@gmail.com>
2025-02-05 20:58:21 -0300

    add jwt auth

---
+++ src/infra/http/auth/auth.module.ts
@@ -0,0 +1,28 @@
+import { UserRepository } from '@/domain/application/repositories/user.repository'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { PrismaUserRepository } from '@/infra/database/prisma/repositories/prisma-user.repository'
+import { Module } from '@nestjs/common'
+import { JwtModule } from '@nestjs/jwt'
+import { PassportModule } from '@nestjs/passport'
+import { JwtStrategy } from './jwt.strategy'
+
+@Module({
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: {
+        expiresIn: '1d',
+      },
+    }),
+],
+  providers: [
+    PrismaService,
+    {
+        provide: UserRepository,
+        useClass: PrismaUserRepository
+    },
+    JwtStrategy
+  ],
+})
+export class AuthModule {}
---
+++ src/infra/http/auth/jwt.strategy.ts
@@ -0,0 +1,20 @@
+import { UserRepository } from '@/domain/application/repositories/user.repository'
+import { PassportStrategy } from "@nestjs/passport"
+import { ExtractJwt, Strategy } from "passport-jwt"
+import { Injectable } from '@nestjs/common'
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private userRepository: UserRepository) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_SECRET,
+    })
+  }
+
+  async validate(payload: any) {
+    const user = await this.userRepository.findByEmail(payload.email)
+    return user
+  }
+}
